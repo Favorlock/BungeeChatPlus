49a50d32c7d9ff4fc0da632721722a76fffbd048
Evan Lindsay <evanlindsay@live.com>
2013-06-19 05:07:08 -0700

    Update existing JUnit test.

--- src/test/java/testing/BaseUnit.java
+++ src/test/java/testing/BaseUnit.java
@@ -1,6 +1,7 @@
 package testing;
 
 import com.gmail.favorlock.bungeechatplus.BungeeChatPlus;
+import com.gmail.favorlock.bungeechatplus.ChannelManager;
 import com.gmail.favorlock.bungeechatplus.ChatterManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.PluginDescription;
@@ -12,12 +13,14 @@ public abstract class BaseUnit {
 	public PluginDescription pdf;
 	public BungeeChatPlus plugin;
 	public ChatterManager chatterManager;
+    public ChannelManager channelManager;
 	public ProxiedPlayer player;
 	
 	public BaseUnit() {
 		pdf = mock(PluginDescription.class);
 		plugin = mock(BungeeChatPlus.class);
 		chatterManager = mock(ChatterManager.class);
+        channelManager = mock(ChannelManager.class);
 		player = mock(ProxiedPlayer.class);
 	}
 
--- src/test/java/testing/commands/VerboseTest.java
+++ src/test/java/testing/commands/VerboseTest.java
@@ -1,11 +1,20 @@
 package testing.commands;
 
+import com.gmail.favorlock.bungeechatplus.cmd.commands.Verbose;
+import com.gmail.favorlock.bungeechatplus.config.BungeeChatPlusConfig;
+import com.gmail.favorlock.bungeechatplus.config.ChannelStorage;
+import com.gmail.favorlock.bungeechatplus.config.ChatterStorage;
+import com.gmail.favorlock.bungeechatplus.entities.Channel;
+import com.gmail.favorlock.bungeechatplus.entities.Chatter;
+import com.gmail.favorlock.bungeechatplus.utils.FontFormat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.stubbing.OngoingStubbing;
 import org.powermock.modules.junit4.PowerMockRunner;
 import testing.BaseUnit;
 
 // Let's import Mockito statically so that the code looks clearer
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
 public class VerboseTest extends BaseUnit {
@@ -13,33 +22,45 @@ public class VerboseTest extends BaseUnit {
 	@Test
 	public void execute() {
 		// When pdf.getName() is called return "BungeeChatPlus"
-//		when(pdf.getName()).thenReturn("BungeeChatPlus");
+		when(pdf.getName()).thenReturn("BungeeChatPlus");
 	    // When plugin.getDescription() is called return pdf
-//	    when(plugin.getDescription()).thenReturn(pdf);
+	    when(plugin.getDescription()).thenReturn(pdf);
 	    // Let's initialize our config
-//	    BungeeChatPlusConfig config = new BungeeChatPlusConfig(plugin);
+	    BungeeChatPlusConfig config = new BungeeChatPlusConfig(plugin);
 	    // When plugin.getConfig() return config
-//	    when(plugin.getConfig()).thenReturn(config);
+	    when(plugin.getConfig()).thenReturn(config);
 	    // When player.getName() return "Favorlock"
-//	    when(player.getName()).thenReturn("Favorlock");
+	    when(player.getName()).thenReturn("Favorlock");
 	    // Lets create our chatter config
-//	    ChatterStorage storage = new ChatterStorage(plugin, player.getName());
-	    // Let's create our test chatter
-//	    Chatter chatter = new Chatter("Favorlock", storage);
+	    ChatterStorage storage = new ChatterStorage(plugin, player.getName());
+        // Lets create our channel config
+        ChannelStorage channelStorage = new ChannelStorage(plugin, "Global");
+        // Lets create our channel
+        Channel channel = new Channel(channelStorage);
+        // When plugin.getChannelManager()
+        when(plugin.getChannelManager()).thenReturn(channelManager);
+        // When channelManager.getChannel("Global");
+        when(channelManager.getChannel("Global")).thenReturn(channel);
+        // Let's create our test chatter
+	    Chatter chatter = new Chatter("Favorlock", storage);
 	    // When plugin.getChatterManager
-//	    when(plugin.getChatterManager()).thenReturn(chatterManager);
+	    when(plugin.getChatterManager()).thenReturn(chatterManager);
 	    // When plugin.getChatter("Favorlock") return chatter
-//	    when(plugin.getChatterManager().getChatter("Favorlock")).thenReturn(chatter);
+	    when(plugin.getChatterManager().getChatter("Favorlock")).thenReturn(chatter);
 	    // Initialize our command
-//	    Verbose command = new Verbose(plugin);;
-	    // Execute the command
-//	    command.execute(player, "bcp verbose", new String[0]);
+	    Verbose command = new Verbose(plugin);
+        // Set chatter.Verbose;
+        chatter.setVerbose(true);
+        // Execute the command
+	    command.execute(player, "bcp verbose", new String[0]);
 	    // Verify the string
-//	    verify(player).sendMessage(FontFormat.translateString("&7You are no longer talking in &2Global Chat"));
+	    verify(player).sendMessage(FontFormat.translateString("&7You are no longer talking in &2Global Chat"));
+        // Set chatter.Verbose;
+        chatter.setVerbose(false);
 	    // Execute the command once more
-//	    command.execute(player, "bcp verbose", new String[0]);
-	    // Verify the string
-//	    verify(player).sendMessage(FontFormat.translateString("&7You are now talking in &2Global Chat"));
+	    command.execute(player, "bcp verbose", new String[0]);
+        // Verify the string
+	    verify(player).sendMessage(FontFormat.translateString("&7You are now talking in &2Global Chat"));
 	}
 
 }
